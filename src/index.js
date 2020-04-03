5cd4562e10f90b21ac814108458c5ee167a2a994
Kenny Lam <kennylam@protonmail.com>
2020-04-03 08:38:12 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import { DotcomShell } from "@carbon/ibmdotcom-vanilla";
+
+const dotcomShellProps = {
+  masthead: {
+    navigation: "default",
+    platform: {
+      name: "IBM Cloud",
+      url: "https://www.ibm.com/cloud"
+    },
+    searchProps: {
+      hasSearch: false
+    }
+  },
+  footer: {
+    footerType: "short"
+  }
+};
+
+const content = `
+    <h1>Hello Vanilla!</h1>
+    <div>
+    We use Parcel to bundle this sandbox, you can find more info about Parcel
+    <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+    </div>
+`;
+
+async function _loadDotcomShell() {
+  const template = await DotcomShell.getDotcomShellWithData({
+    content,
+    ...dotcomShellProps
+  });
+  const yourapp = document.getElementById("app");
+  yourapp.innerHTML = template;
+  DotcomShell.init(yourapp);
+}
+
+_loadDotcomShell();
