b437d87a5b965fccc5732dbc9908993d45859131
Kenny Lam <kennylam@protonmail.com>
2020-05-29 13:16:11 -0400

    chore(data): add dummy content

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import content from "./data/content";
 import { DotcomShell } from "@carbon/ibmdotcom-vanilla";
 import "./styles.scss";
 
@@ -18,14 +19,6 @@ const dotcomShellProps = {
     footerType: "tall",
   },
 };
-
-const content = `
-    <h1>Hello Vanilla!</h1>
-    <div>
-    We use Parcel to bundle this sandbox, you can find more info about Parcel
-    <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-    </div>
-`;
 
 async function _loadDotcomShell() {
   const template = await DotcomShell.getDotcomShellWithData({
